730c83562eee915168824e1e3d270d424d710751
Mero0077 <omartalaatsaad2017@gmail.com>
2025-07-14 13:43:30 +0300

    AddingGeneralRepo& CQRS

---
+++ Domain/IRepositories/IGeneralRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.IRepositories
+{
+    public interface IGeneralRepository<T>
+    {
+        public IQueryable<T> GetAll();
+
+        public IQueryable<T> Get(Expression<Func<T, bool>> expression);
+
+        public  Task<T> GetOneWithTrackingAsync(Expression<Func<T, bool>> expression);
+
+        public  Task<T> GetOneByIdAsync(int Id);
+
+        public  Task<T> AddAsync(T entity);
+
+        public  Task<T> UpdateAsync(T entity);
+
+        public  Task UpdateIncludeAsync(T entity, params string[] modifiedProperties);
+
+        public  Task<T> DeleteAsync(int Id);
+
+        public bool IsExists(int Id);
+
+        public  Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+
+        public  Task<bool> AnyAsync(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken = default);
+
+    }
+}
---
+++ Domain/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class User:BaseModel
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Country { get; set; }
+        public string UserName { get; set; }
+    }
+}
