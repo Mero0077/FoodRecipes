71666c92c3ad2105abe9a013b4610f282839c863
Mero0077 <omartalaatsaad2017@gmail.com>
2025-07-15 14:24:20 +0300

    addResetPassword&Jwt

---
+++ Presentation/ViewModels/User/ResetPasswordVM.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace Presentation.ViewModels.User
+{
+    public class ResetPasswordVM
+    {
+        [Required]
+        public string UserName  { get; set; }
+        [Required]
+        public string OldPassword { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password required")]
+        [CompareAttribute("NewPassword", ErrorMessage = "Password doesn't match.")]
+        public string NewPassword { get; set; }
+    }
+}
